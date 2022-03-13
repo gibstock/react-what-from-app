00d4664bf88c0f40529e69bb54de6eb03a235472
Andre Gonzales <gibstock@gmail.com>
2022-03-12 21:27:16 -0800

    update css styling add to top button

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Loader = () => {
   return(
     <div className='flex-center full'>
-      <span className='loading-bar'></span>
+      <span className='loading-bar'>
+        <span className='loading-fill'></span>
+      </span>
       <span className='loading-text'>Loading...</span>
     </div>
   )
