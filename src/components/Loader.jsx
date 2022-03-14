684b60a23e5566dbd4531c15248429dee58f8e51
Andre Gonzales <gibstock@gmail.com>
2022-03-14 12:32:46 -0700

    updated manifest.json icons and loading animation

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -11,6 +11,9 @@ const Loader = () => {
         <span id='l-dot-one'>.</span>
         <span id='l-dot-two'>.</span>
         <span id='l-dot-three'>.</span>
+        <span id='l-dot-four'>.</span>
+        <span id='l-dot-five'>.</span>
+        <span id='l-dot-six'>.</span>
       </div>
     </div>
   )
